ad5afdd73614d69667f8848f3fbaf3dc0e125ea2
Brian Miller <brian@parameter1.com>
2021-07-28 06:43:24 -0500

    p1 dep updates

--- packages/minexpo/browser/index.js
+++ packages/minexpo/browser/index.js
@@ -1,9 +1,10 @@
 import Directory from '@ascend-media/package-directory/browser';
 import Inquiry from '@parameter1/base-cms-marko-web-inquiry/browser';
-import paginationPageNumberInput from './pagination-page-number-input.vue';
+
+const PaginationPageNumberInput = () => import(/* webpackChunkName: "minexpo-pagination-page-number-input" */ './pagination-page-number-input.vue');
 
 export default (Browser) => {
   Directory(Browser);
   Inquiry(Browser);
-  Browser.registerComponent('PaginationPageNumberInput', paginationPageNumberInput);
+  Browser.registerComponent('PaginationPageNumberInput', PaginationPageNumberInput);
 };
