0dda49e5539f412dd4b75111e3330a918efc878e
Shinsina <shinsina@att.net>
2021-08-17 08:22:55 -0500

    Add site config files

---
+++ sites/aao.hns.org/config/site.js
@@ -0,0 +1,44 @@
+const navigation = require('./navigation');
+const gam = require('./gam');
+const logos = require('./logos');
+
+module.exports = {
+  logos,
+  navigation,
+  gam,
+  company: 'Ascend Media',
+  copyrightNotice: '',
+  sponsoredText: 'Paid Advertising Content',
+  socialMediaLinks: [
+    { provider: 'facebook', href: '#', target: '_blank' },
+    { provider: 'instagram', href: '#', target: '_blank' },
+    { provider: 'twitter', href: '#', target: '_blank' },
+  ],
+  gtm: {
+    containerId: 'GTM-PWX9TTJ',
+  },
+  twitterFeed: {
+    href: '',
+    tagline: '',
+  },
+  gcse: {
+    id: '',
+  },
+  contactUs: {
+    to: 'DSanford@ascendintegratedmedia.com',
+    branding: {
+      logo: '',
+    },
+    support: {
+      email: 'reply@hearthubs.org',
+    },
+  },
+  inquiry: {
+    enabled: true,
+    directSend: true,
+    sendTo: 'DSanford@ascendintegratedmedia.com',
+    sendFrom: 'AAD <noreply@ascendintegratedmedia.com>',
+    logo: '',
+    bgColor: '#ffffff',
+  },
+};
