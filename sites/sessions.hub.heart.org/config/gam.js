62f1b8cfb7a8a324b93d2a9a1e3fb486fb120d07
Shinsina <shinsina@att.net>
2021-09-15 10:24:29 -0500

    Add rail3 to GAM configuration Sessions.

--- sites/sessions.hub.heart.org/config/gam.js
+++ sites/sessions.hub.heart.org/config/gam.js
@@ -8,6 +8,7 @@ config
     { name: 'lb2', templateName: 'LB', path: 'lb2' },
     { name: 'rail1', templateName: 'RAIL1', path: 'rail1' },
     { name: 'rail2', templateName: 'RAIL1', path: 'rail2' },
+    { name: 'rail3', templateName: 'RAIL1', path: 'rail3' },
     { name: 'load-more', templateName: 'RAIL1', path: 'load-more' },
   ]);
 
