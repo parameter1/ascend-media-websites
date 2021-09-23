81ac8cb57538a1d4a9e2a37790c66e13e4f13624
Shinsina <shinsina@att.net>
2021-09-23 12:04:27 -0500

    Remove Need URL comment

--- sites/aao.hns.org/config/navigation.js
+++ sites/aao.hns.org/config/navigation.js
@@ -20,7 +20,7 @@ const topics = [
 
 const secondary = [
   { href: 'https://otomtg21.junolive.co/Welcome', label: 'Virtual Platform', target: '_blank' },
-  { href: 'https://www.researchposters.com/display_posters.aspx?code=AAO2021', label: 'Posters', target: '_blank' }, // Need URL
+  { href: 'https://www.researchposters.com/display_posters.aspx?code=AAO2021', label: 'Posters', target: '_blank' },
   // { href: '#', label: 'Abstracts', target: '_blank' },
   // { href: '#', label: 'Claim CME', target: '_blank' },
 ];
