770344686d1f3c50f9bc4f59b903f1371b2c1317
Shinsina <shinsina@att.net>
2021-10-04 14:14:17 -0500

    Change labels and aliases for sections.

--- sites/asa.org/config/navigation.js
+++ sites/asa.org/config/navigation.js
@@ -11,17 +11,17 @@ const userTools = [
 ];
 
 const topics = [
-  { href: '/day-1', label: 'Day 1' },
-  { href: '/day-2', label: 'Day 2' },
-  { href: '/day-3', label: 'Day 3' },
-  { href: '/day-4', label: 'Day 4' },
-  { href: '/day-5', label: 'Day 5' },
+  { href: '/friday', label: 'Friday' },
+  { href: '/saturday', label: 'Saturday' },
+  { href: '/sunday', label: 'Sunday' },
+  { href: '/monday', label: 'Monday' },
+  { href: '/tuesday', label: 'Tuesday' },
   { href: 'https://www.asahq.org/annualmeeting', label: 'Meeting Info', target: '_blank' },
   { href: '#', label: 'Claim CME', target: '_blank' }, // NEED URL
 ];
 
 const resources = [
-  { href: 'https://www.abstractsonline.com/pp8/#!/9323', label: 'Program', target: '_blank' },
+  { href: 'https://www.abstractsonline.com/pp8/#!/9323', label: 'Sessions', target: '_blank' },
   { href: 'https://www.asahq.org/', label: 'On-Demand', target: '_blank' }, // Will be different offsite link when given
 ];
 
