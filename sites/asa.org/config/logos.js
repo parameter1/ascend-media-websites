76f5cfc24fc8638eb18cd9e8e6ae1ad766fd30fa
Shinsina <shinsina@att.net>
2021-10-04 08:49:20 -0500

    Update header logo.

--- sites/asa.org/config/logos.js
+++ sites/asa.org/config/logos.js
@@ -1,11 +1,11 @@
 module.exports = {
   navbar: {
-    src: 'https://img.ascendmedia.com/files/base/ascend/hh/image/static/asa/asa-header.png?h=200',
+    src: 'https://img.ascendmedia.com/files/base/ascend/hh/image/static/asa/ASA_header__logo__v2.png?h=200',
     srcset: [
-      'https://img.ascendmedia.com/files/base/ascend/hh/image/static/asa/asa-header.png?h=400 2x',
+      'https://img.ascendmedia.com/files/base/ascend/hh/image/static/asa/ASA_header__logo__v2.png?h=400 2x',
     ],
     href: '/',
-    title: 'Example Site',
+    title: 'ASA Anesthesiology Today',
   },
   footer: {
     src: 'https://img.ascendmedia.com/files/base/ascend/hh/image/static/asa/asa-footer-white.png?h=200',
