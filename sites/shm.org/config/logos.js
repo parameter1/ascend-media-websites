e03713720868eee114c4df692e18b50381084578
Shinsina <shinsina@att.net>
2021-10-18 13:31:54 -0500

    Create config files.

---
+++ sites/shm.org/config/logos.js
@@ -0,0 +1,16 @@
+module.exports = {
+  navbar: {
+    src: 'https://placehold.jp/200x100.png',
+    srcset: [
+      'https://placehold.jp/200x100.png',
+    ],
+    href: '/',
+    title: 'SHM',
+  },
+  footer: {
+    src: 'https://placehold.jp/200x100.png',
+    srcset: [
+      'https://placehold.jp/200x100.png',
+    ],
+  },
+};
---
+++ sites/shm.org/config/site.js
@@ -0,0 +1,45 @@
+const navigation = require('./navigation');
+const gam = require('./gam');
+const logos = require('./logos');
+
+module.exports = {
+  logos,
+  navigation,
+  gam,
+  company: 'Ascend Media',
+  sponsoredText: 'Paid Advertising Content',
+  socialMediaLinks: [
+    { provider: 'twitter', href: '', target: '_blank' },
+    { provider: 'facebook', href: '', target: '_blank' },
+    { provider: 'instagram', href: '', target: '_blank' },
+    { provider: 'youtube', href: '', target: '_blank' },
+    { provider: 'linkedin', href: '', target: '_blank' },
+  ],
+  gtm: {
+    containerId: 'GTM-5LK3FQ6',
+  },
+  twitterFeed: {
+    href: '',
+    tagline: 'Tweets by ',
+  },
+  gcse: {
+    id: '2d2e730087c2bd49e',
+  },
+  contactUs: {
+    to: 'DSanford@ascendintegratedmedia.com',
+    branding: {
+      logo: 'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aua/site_logo.png?h=50',
+    },
+    support: {
+      email: 'reply@hearthubs.org',
+    },
+  },
+  inquiry: {
+    enabled: true,
+    directSend: true,
+    sendTo: 'DSanford@ascendintegratedmedia.com',
+    sendFrom: 'AUA <noreply@ascendintegratedmedia.com>',
+    logo: 'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aua/site_logo.png?h=50',
+    bgColor: '#ffffff',
+  },
+};
