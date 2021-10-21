1c10afe0ae42a8991ce24627e58efa4930ceb0a2
Brian Miller <brian@parameter1.com>
2021-10-21 14:17:52 -0500

    Remove Pagination & unused components

--- sites/minexpo.com/index.js
+++ sites/minexpo.com/index.js
@@ -1,7 +1,6 @@
 const newrelic = require('newrelic');
 const { startServer } = require('@parameter1/base-cms-marko-web');
 const { set, getAsObject } = require('@parameter1/base-cms-object-path');
-const paginated = require('@ascend-media/package-minexpo/middleware/paginated');
 const { version } = require('./package.json');
 const routes = require('./server/routes');
 const siteConfig = require('./config/site');
@@ -11,7 +10,6 @@ const components = require('./server/components');
 const fragments = require('./server/fragments');
 const redirectHandler = require('./redirect-handler');
 
-const { env } = process;
 const { log } = console;
 
 module.exports = startServer({
@@ -29,8 +27,6 @@ module.exports = startServer({
     // Setup GAM.
     const gamConfig = getAsObject(siteConfig, 'gam');
     if (gamConfig) set(app.locals, 'GAM', gamConfig);
-
-    app.use(paginated());
   },
   onAsyncBlockError: e => newrelic.noticeError(e),
   redirectHandler,
