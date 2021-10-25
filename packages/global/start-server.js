e5e991ea2fb6caf6101983048381d8d1808f1a70
Shinsina <shinsina@att.net>
2021-10-25 13:48:20 -0500

    Bring back old error page for isc and sessions.

--- packages/global/start-server.js
+++ packages/global/start-server.js
@@ -22,6 +22,7 @@ module.exports = (options = {}) => {
     document: options.document || document,
     components: options.components || components,
     fragments: options.fragments || fragments,
+    errorTemplate: options.errorTemplate,
     onStart: async (app) => {
       if (typeof onStart === 'function') await onStart(app);
       app.set('trust proxy', 'loopback, linklocal, uniquelocal');
