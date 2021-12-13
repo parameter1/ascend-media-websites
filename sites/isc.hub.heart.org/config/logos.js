a77a5fb2d07ced787c1cf7dc697b4b8165735107
Shinsina <shinsina@att.net>
2021-12-13 14:04:59 -0600

    Change the logo query variables to fit theme.

--- sites/isc.hub.heart.org/config/logos.js
+++ sites/isc.hub.heart.org/config/logos.js
@@ -1,8 +1,8 @@
 module.exports = {
   navbar: {
-    src: 'https://img.hub.heart.org/files/base/ascend/hearthub/image/static/isc-logo.png?h=90&auto=format,compress&q=70',
+    src: 'https://img.hub.heart.org/files/base/ascend/hearthub/image/static/isc-logo.png?h=200',
     srcset: [
-      'https://img.hub.heart.org/files/base/ascend/hearthub/image/static/isc-logo.png?h=180&auto=format,compress&q=70 2x',
+      'https://img.hub.heart.org/files/base/ascend/hearthub/image/static/isc-logo.png?h=400 2x',
     ],
     href: '/',
     title: 'Scientific Sessions',
