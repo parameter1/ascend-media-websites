2c7afdaa2e9d866081db7b74c5311f483b136eb7
Shinsina <shinsina@att.net>
2021-12-13 14:04:26 -0600

    Nav additions and corrections.

--- sites/isc.hub.heart.org/config/navigation.js
+++ sites/isc.hub.heart.org/config/navigation.js
@@ -12,7 +12,7 @@ const userTools = [
 ];
 
 const topics = [
-  { href: '/late-breaking-science', label: 'Late-Breaking Science' },
+  { href: '/late-breaking-science', label: 'Late-Breaking-Science' },
   { href: '/daily-coverage', label: 'Daily Coverage' },
   { href: '/industry-highlights', label: 'Industry Highlights' },
   { href: 'https://eventpilotadmin.com/web/planner.php?id=ISC21', label: 'Program' },
@@ -20,6 +20,7 @@ const topics = [
 ];
 
 const secondary = [
+  { href: 'https://twitter.com/search?q=%23ISC22&src=typed_query', label: 'ISC#22', target: '_blank' },
   // { href: 'https://isc.heart.org', label: 'Virtual Experience Access', target: '_blank' },
   // { href: ' https://asa.apprisor.org', label: 'Posters Access', target: '_blank' },
 ];
