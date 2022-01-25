57bf29992f1d12c5176c9713ccd338c5d2bd428e
Shinsina <shinsina@att.net>
2022-01-25 13:11:29 -0600

    Update logos.

--- sites/shm.org/config/logos.js
+++ sites/shm.org/config/logos.js
@@ -1,16 +1,16 @@
 module.exports = {
   navbar: {
-    src: 'https://placehold.jp/200x100.png',
+    src: 'https://img.ascendmedia.com/files/base/ascend/hh/image/static/shm/SHM_MNC_logo.png?h=200',
     srcset: [
-      'https://placehold.jp/200x100.png',
+      'https://img.ascendmedia.com/files/base/ascend/hh/image/static/shm/SHM_MNC_logo.png?h=400 2x',
     ],
     href: '/',
     title: 'SHM',
   },
   footer: {
-    src: 'https://placehold.jp/200x100.png',
+    src: 'https://img.ascendmedia.com/files/base/ascend/hh/image/static/shm/SHMC22_logo_white.png?h=200',
     srcset: [
-      'https://placehold.jp/200x100.png',
+      'https://img.ascendmedia.com/files/base/ascend/hh/image/static/shm/SHMC22_logo_white.png?h=400 2x',
     ],
   },
 };
