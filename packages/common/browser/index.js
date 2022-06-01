2deb45a51ace5c4c22f124560adcacec5eb89fd4
Brian Miller <brian@parameter1.com>
2022-05-31 19:08:30 -0500

    remove contact us package and common package refs
    Remove the contact us packaged and remove the contact us parts setup withing the common package.  This will be replaced by the updated @parameter1/base-cms-marko-web-contact-us package.

--- packages/common/browser/index.js
+++ packages/common/browser/index.js
@@ -1,7 +1,5 @@
 import Leaders from '@parameter1/base-cms-marko-web-leaders/browser';
-
 const ImageSlider = () => import(/* webpackChunkName: "common-image-slider" */ './image-slider.vue');
-const ContactUsForm = () => import(/* webpackChunkName: "common-contact-us" */ './contact-us-form.vue');
 const CommonGTMTrackLeaders = () => import(/* webpackChunkName: "common-gtm-track-leaders" */ './gtm-track-leaders.vue');
 const CommonLeadersCompanyWebsiteLink = () => import(/* webpackChunkName: "common-leaders-company-website-link" */ './leaders-company-website-link.vue');
 const CommonLeadersCompanySocialLink = () => import(/* webpackChunkName: "common-leaders-company-social-link" */ './leaders-company-social-link.vue');
@@ -18,6 +16,6 @@ export default (Browser) => {
   Leaders(Browser);
 
   // @todo this should be removed once contact us is moved to core.
-  Browser.register('CommonContactUsForm', ContactUsForm);
+  // Browser.register('CommonContactUsForm', ContactUsForm);
   Browser.register('CommonImageSlider', ImageSlider);
 };
