6307c4455cb2a32567ea35f36acbad40e0841594
Shinsina <shinsina@att.net>
2022-06-28 11:53:59 -0500

    Unify naming convention of Vue browser components (prefix with Minexpo).

--- packages/minexpo/browser/index.js
+++ packages/minexpo/browser/index.js
@@ -2,17 +2,17 @@ import ContactUs from '@parameter1/base-cms-marko-web-contact-us/browser';
 import Inquiry from '@parameter1/base-cms-marko-web-inquiry/browser';
 import Leaders from '@parameter1/base-cms-marko-web-leaders/browser';
 
-const ImageSlider = () => import(/* webpackChunkName: "shared-image-slider" */ './image-slider.vue');
-const MinexpoGTMTrackLeaders = () => import(/* webpackChunkName: "shared-gtm-track-leaders" */ './gtm-track-leaders.vue');
-const MinexpoLeadersCompanyWebsiteLink = () => import(/* webpackChunkName: "common-leaders-company-website-link" */ './leaders-company-website-link.vue');
-const MinexpoLeadersCompanySocialLink = () => import(/* webpackChunkName: "common-leaders-company-social-link" */ './leaders-company-social-link.vue');
-const PaginationPageNumberInput = () => import(/* webpackChunkName: "shared-pagination-page-number-input" */ './pagination-page-number-input.vue');
-const AutoScroll = () => import(/* webpackChunkName: "directory-auto-scroll" */ './auto-scroll.vue');
+const ImageSlider = () => import(/* webpackChunkName: "minexpo-image-slider" */ './image-slider.vue');
+const MinexpoGTMTrackLeaders = () => import(/* webpackChunkName: "minexpo-gtm-track-leaders" */ './gtm-track-leaders.vue');
+const MinexpoLeadersCompanyWebsiteLink = () => import(/* webpackChunkName: "minexpo-leaders-company-website-link" */ './leaders-company-website-link.vue');
+const MinexpoLeadersCompanySocialLink = () => import(/* webpackChunkName: "minexpo-leaders-company-social-link" */ './leaders-company-social-link.vue');
+const PaginationPageNumberInput = () => import(/* webpackChunkName: "minexpo-pagination-page-number-input" */ './pagination-page-number-input.vue');
+const AutoScroll = () => import(/* webpackChunkName: "minexpo-auto-scroll" */ './auto-scroll.vue');
 
 export default (Browser) => {
   ContactUs(Browser);
   Inquiry(Browser);
-  Browser.registerComponent('PaginationPageNumberInput', PaginationPageNumberInput);
+  Browser.registerComponent('MinexpoPaginationPageNumberInput', PaginationPageNumberInput);
   const { EventBus } = Browser;
   Browser.registerComponent('MinexpoGTMTrackLeaders', MinexpoGTMTrackLeaders, { provide: { EventBus } });
   Browser.registerComponent('MinexpoLeadersCompanyWebsiteLink', MinexpoLeadersCompanyWebsiteLink, {
@@ -23,6 +23,6 @@ export default (Browser) => {
   });
   Leaders(Browser);
 
-  Browser.register('CommonImageSlider', ImageSlider);
-  Browser.registerComponent('DirectoryAutoScroll', AutoScroll);
+  Browser.register('MinexpoImageSlider', ImageSlider);
+  Browser.registerComponent('MinexpoAutoScroll', AutoScroll);
 };
