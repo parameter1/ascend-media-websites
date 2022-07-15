1bb3e964d351ce50938e7544a96314b86d599456
Shinsina <shinsina@att.net>
2022-07-15 11:00:02 -0500

    Update header logo to 2022 version

--- sites/acep.org/config/logos.js
+++ sites/acep.org/config/logos.js
@@ -1,8 +1,8 @@
 module.exports = {
   navbar: {
-    src: 'https://img.ascendmedia.com/files/base/ascend/hh/image/static/acep/ACEP21-header.jpeg?h=200',
+    src: 'https://img.ascendmedia.com/files/base/ascend/hh/image/static/acep/ACEP22_MNCHeaderGraphic.png?h=200',
     srcset: [
-      'https://img.ascendmedia.com/files/base/ascend/hh/image/static/acep/ACEP21-header.jpeg?h=400 2x',
+      'https://img.ascendmedia.com/files/base/ascend/hh/image/static/acep/ACEP22_MNCHeaderGraphic.png?h=400 2x',
     ],
     href: '/',
     title: 'ACEP',
