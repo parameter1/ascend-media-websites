83be0a946f856ec597967fb808ace3830447db3f
Shinsina <shinsina@att.net>
2022-08-08 14:20:30 -0500

    Configure GAM

--- sites/thoracicam.com/config/gam.js
+++ sites/thoracicam.com/config/gam.js
@@ -1,6 +1,6 @@
 const configureGAM = require('@ascend-media/package-global/config/gam');
 
-const config = configureGAM({ basePath: '' });
+const config = configureGAM({ basePath: 'thoracic-am' });
 
 config
   .setAliasAdUnits('default', [
