22ce0b07a69a5afb10f0a0179cc8d02bef2dc6a3
Jessica Wade <jessica.wade@parameter1.com>
2022-08-18 11:45:00 -0500

    Update ‘Program’ link and deactivate link until future dates

--- sites/sessions.hub.heart.org/config/navigation.js
+++ sites/sessions.hub.heart.org/config/navigation.js
@@ -16,15 +16,15 @@ const topics = [
   { href: '/daily-coverage', label: 'Daily Coverage' },
   { href: '/industry-highlights', label: 'Industry Highlights' },
   { href: '/featured-exhibitors', label: 'Featured Exhibitors' },
-  { href: 'https://eventpilotadmin.com/web/planner.php?id=AHA21', label: 'Program', target: '_blank' },
+  { href: 'https://www.abstractsonline.com/pp8/?_ga=2.20399802.36245607.1660839477-74158399.1585661384#!/10611', label: 'Program', target: '_blank' },
   { href: '/previews', label: 'Previews' },
 ];
 
 const secondary = [
   { href: 'https://twitter.com/search?q=%23AHA21&src=typed_query', label: '#AHA22' },
-  { href: 'https://twitter.com/hashtag/ReSS21?src=hashtag_click', label: '#ReSS22' },
-  { href: 'https://AHAsessions.heart.org', label: 'Virtual Experience Access', target: '_blank' },
-  { href: 'https://aha.apprisor.org', label: 'Sessions Posters', target: '_blank' },
+  { href: '#', label: '#ReSS22' },
+  { href: '#', label: 'Virtual Experience Access', target: '_blank' },
+  { href: '#', label: 'Sessions Posters', target: '_blank' },
 ];
 
 module.exports = {
