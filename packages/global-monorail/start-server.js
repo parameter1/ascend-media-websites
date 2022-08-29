e991b93aeb97bc7e4d9f90af2cd46e9460111886
Brandon Krigbaum <brandonbk@gmail.com>
2022-08-29 07:54:50 -0500

    Add global-monorail package

---
+++ packages/global-monorail/start-server.js
@@ -0,0 +1,67 @@
+const newrelic = require('newrelic');
+const { startServer } = require('@parameter1/base-cms-marko-web');
+const { set, get } = require('@parameter1/base-cms-object-path');
+const loadInquiry = require('@parameter1/base-cms-marko-web-inquiry');
+const htmlSitemapPagination = require('@parameter1/base-cms-marko-web-html-sitemap/middleware/paginated');
+
+const document = require('./components/document');
+const components = require('./components');
+const fragments = require('./fragments');
+const sharedRoutes = require('./routes');
+const paginated = require('./middleware/paginated');
+const oembedHandler = require('./oembed-handler');
+
+const routes = (siteRoutes, siteConfig) => (app) => {
+  // Handle submissions on /__inquiry
+  loadInquiry(app);
+  // Shared/global routes (all sites)
+  sharedRoutes(app, siteConfig);
+  // Load site routes
+  siteRoutes(app);
+};
+
+module.exports = (options = {}) => {
+  const { onStart } = options;
+  const googleNewsInput = {
+    days: 7,
+    includeContentTypes: ['Article'],
+    excludeLabels: ['Sponsored', 'Sponsored by RoadPro'],
+  };
+  return startServer({
+    ...options,
+    routes: routes(options.routes, options.siteConfig),
+    document: options.document || document,
+    components: options.components || components,
+    fragments: options.fragments || fragments,
+    sitemapsHeaders: {
+      'x-google-news-input': JSON.stringify(googleNewsInput),
+    },
+    onStart: async (app) => {
+      if (typeof onStart === 'function') await onStart(app);
+      app.set('trust proxy', 'loopback, linklocal, uniquelocal');
+
+      // Use paginated middleware
+      app.use(paginated());
+
+      // Use paginated middleware
+      app.use(htmlSitemapPagination());
+
+      // Setup GAM.
+      const gamConfig = get(options, 'siteConfig.gam');
+      set(app.locals, 'GAM', gamConfig);
+
+      // Setup NativeX.
+      const nativeXConfig = get(options, 'siteConfig.nativeX');
+      set(app.locals, 'nativeX', nativeXConfig);
+
+      // i18n
+      const i18n = v => v;
+      set(app.locals, 'i18n', options.i18n || i18n);
+    },
+    onAsyncBlockError: e => newrelic.noticeError(e),
+
+    embeddedMediaHandlers: {
+      oembed: oembedHandler,
+    },
+  });
+};
