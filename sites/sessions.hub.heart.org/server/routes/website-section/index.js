97fa9beefce6908b00126fb147ed3cd3430ce011
Shinsina <shinsina@att.net>
2022-10-06 14:14:43 -0500

    Use template for industry-highlights, Sessions.

--- sites/sessions.hub.heart.org/server/routes/website-section/index.js
+++ sites/sessions.hub.heart.org/server/routes/website-section/index.js
@@ -1,11 +1,17 @@
 const { withWebsiteSection } = require('@parameter1/base-cms-marko-web/middleware');
 const queryFragment = require('@ascend-media/package-global/graphql/fragments/website-section-page');
 const section = require('@ascend-media/package-global/templates/website-section');
+const nativeX = require('@ascend-media/package-global/templates/website-section/native-x');
 const exhibitors = require('../../templates/website-section/featured-exhibitors');
 
 module.exports = (app) => {
   app.get('/:alias(featured-exhibitors)', withWebsiteSection({
     template: exhibitors,
+    queryFragment,
+  }));
+
+  app.get('/:alias(industry-highlights)', withWebsiteSection({
+    template: nativeX,
     queryFragment,
   }));
 
