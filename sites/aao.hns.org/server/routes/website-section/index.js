24fbf553f5e544bc0d132a82a8011d3ca8906fba
Shinsina <shinsina@att.net>
2022-10-11 14:24:14 -0500

    Update routing to use site specific templates

--- sites/aao.hns.org/server/routes/website-section/index.js
+++ sites/aao.hns.org/server/routes/website-section/index.js
@@ -1,6 +1,6 @@
 const { withWebsiteSection } = require('@parameter1/base-cms-marko-web/middleware');
 const queryFragment = require('@ascend-media/package-global/graphql/fragments/website-section-page');
-const section = require('@ascend-media/package-global/templates/website-section');
+const section = require('../../templates/website-section');
 
 module.exports = (app) => {
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
