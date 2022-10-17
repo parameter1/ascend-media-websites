24b369c15b676db3f602e86669eb9c922f57d90f
Shinsina <shinsina@att.net>
2022-10-17 10:13:33 -0500

    Configure Native-X

---
+++ sites/bulletin.entnet.org/config/native-x.js
@@ -0,0 +1,7 @@
+module.exports = {
+  placements: {
+    default: '634d704a311875000158941f',
+    employment: '634d707ae058a900017a0a1c',
+    'courses-meetings': '634d70a1e058a900017a0ab3',
+  },
+};
