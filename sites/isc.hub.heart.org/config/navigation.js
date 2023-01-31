59fe3bd9b80de1e52b141245bf650874640be777
Jessica Wade <jessica.wade@parameter1.com>
2023-01-30 21:50:55 -0600

    Update ePoster url

--- sites/isc.hub.heart.org/config/navigation.js
+++ sites/isc.hub.heart.org/config/navigation.js
@@ -21,7 +21,7 @@ const topics = [
 const secondary = [
   { href: 'https://twitter.com/search?q=%23ISC22&src=typed_query', label: '#ISC23', target: '_blank' },
   { href: 'https://isc23.heart.org/', label: 'Access Virtual Experience', target: '_blank' },
-  { href: 'https://asa.apprisor.org/epsWelcome.cfm?CFID=1335453&CFTOKEN=d5596cdbf0141878-1C52E3F4-B8FF-86F5-423FA43422D0A1CF', label: 'Access ePosters', target: '_blank' },
+  { href: 'https://asa.apprisor.org/epsWelcome.cfm', label: 'Access ePosters', target: '_blank' },
 ];
 
 module.exports = {
