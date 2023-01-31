99fb09a9c5bd876b288ea97cd51a95c275e3f022
Jessica Wade <jessica.wade@parameter1.com>
2023-01-31 17:14:44 -0600

    Update ‘privacy policy’ & ‘terms & conditions’

--- sites/isc.hub.heart.org/config/navigation.js
+++ sites/isc.hub.heart.org/config/navigation.js
@@ -6,8 +6,8 @@ const searchItem = {
 };
 
 const userTools = [
-  { href: '/page/privacy-policy', label: 'Privacy Policy' },
-  { href: '/page/terms-conditions', label: 'Terms & Conditions' },
+  { href: 'https://www.heart.org/en/about-us/statements-and-policies/privacy-statement', label: 'Privacy Policy' },
+  { href: 'https://professional.heart.org/en/meetings/international-stroke-conference/registration/terms-and-conditions', label: 'Terms & Conditions' },
   { href: '/page/accessibility-statement', label: 'Accessibility Statement', target: '_blank' },
 ];
 
@@ -22,6 +22,7 @@ const secondary = [
   { href: 'https://twitter.com/search?q=%23ISC22&src=typed_query', label: '#ISC23', target: '_blank' },
   { href: 'https://isc23.heart.org/', label: 'Access Virtual Experience', target: '_blank' },
   { href: 'https://asa.apprisor.org/epsWelcome.cfm', label: 'Access ePosters', target: '_blank' },
+  { href: 'https://www.heart.org/en/about-us/statements-and-policies/privacy-statement', label: 'Privacy Policy' },
 ];
 
 module.exports = {
