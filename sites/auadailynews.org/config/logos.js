a581531efaf288299170d256c1fb60b89c9f3a51
Jessica Wade <jessica.wade@parameter1.com>
2023-03-07 09:50:51 -0600

    Update logos

--- sites/auadailynews.org/config/logos.js
+++ sites/auadailynews.org/config/logos.js
@@ -1,16 +1,16 @@
 module.exports = {
   navbar: {
-    src: 'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aua/aua-header-logo-2022.png?h=100',
+    src: 'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aua/AUA23_MNC_logo.png?h=100',
     srcset: [
-      'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aua/aua-header-logo-2022.png?h=200 2x',
+      'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aua/AUA23_MNC_logo.png?h=200 2x',
     ],
     href: '/',
     title: 'AUA Daily News',
   },
   footer: {
-    src: 'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aua/aua-footer-logo-2022-cropped.png?h=100',
+    src: 'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aua/AUA23_MNC_logo.png?h=100',
     srcset: [
-      'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aua/aua-footer-logo-2022-cropped.png?h=200 2x',
+      'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aua/AUA23_MNC_logo.png?h=200 2x',
     ],
   },
 };
