4a4b797e0872862b1cd6028d0ab0209124e57847
Jake Collins <shinsina@att.net>
2023-05-02 12:24:05 -0500

    Load the ContactUs Vue component

--- packages/global-monorail/browser/index.js
+++ packages/global-monorail/browser/index.js
@@ -1,8 +1,10 @@
 import MonoRail from '@parameter1/base-cms-marko-web-theme-monorail/browser';
+import ContactUs from '@parameter1/base-cms-marko-web-contact-us/browser';
 
 const ContentMeterTrack = () => import(/* webpackChunkName: "content-meter-tracker" */ './track-content-meter.vue');
 
 export default (Browser) => {
   MonoRail(Browser, { enableOmedaIdentityX: false });
+  ContactUs(Browser);
   Browser.register('ContentMeterTrack', ContentMeterTrack);
 };
