806efea2a919b9c5b796125eab325ce3a0d38723
Jake Collins <shinsina@att.net>
2023-05-02 12:25:06 -0500

    Update routing for contact us page

--- sites/ampulmonary.com/server/routes/dynamic-page.js
+++ sites/ampulmonary.com/server/routes/dynamic-page.js
@@ -1,8 +1,13 @@
 const { withDynamicPage } = require('@parameter1/base-cms-marko-web/middleware');
 const queryFragment = require('@parameter1/base-cms-marko-web-theme-monorail/graphql/fragments/dynamic-page');
+const contactUs = require('@ascend-media/package-global-monorail/templates/content/contact-us');
 const page = require('@ascend-media/package-global-monorail/templates/dynamic-page');
 
 module.exports = (app) => {
+  app.get('/page/:alias(contact-us)', withDynamicPage({
+    template: contactUs,
+    queryFragment,
+  }));
   app.get('/page/:alias(*)', withDynamicPage({
     template: page,
     queryFragment,
