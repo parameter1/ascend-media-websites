09d999d0ea5c0a7b9584b0472f0cac2028dde040
Jessica Wade <jessica.wade@parameter1.com>
2023-07-17 14:25:53 -0500

    Update newsletter phone image

--- sites/ampulmonary.com/config/newsletter.js
+++ sites/ampulmonary.com/config/newsletter.js
@@ -13,11 +13,11 @@ const defaults = {
 module.exports = {
   signupBanner: {
     ...defaults,
-    imagePath: 'files/base/smg/all/image/static/newsletter-pushdown/sab-full.png',
+    imagePath: 'files/base/ascend/hh/image/static/pulmonary/pulmonary-newsletter-full.png',
   },
   pushdown: {
     ...defaults,
-    imagePath: 'files/base/smg/all/image/static/newsletter-pushdown/sab-half.png',
+    imagePath: 'files/base/ascend/hh/image/static/pulmonary/pulmonary-newsletter-half.png',
     description: 'amPulmonary enewsletter is the hub of news and cutting-edge information for pulmonary clinicians, researchers, and medical professionals interested in the latest developments.',
   },
 
