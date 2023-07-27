d13c701557cf539874d50d2244bd406f7c8bb850
Shinsina <shinsina@att.net>
2023-07-27 14:05:58 -0500

    Add route for gallery style page

--- sites/sessions.hub.heart.org/server/routes/dynamic-page.js
+++ sites/sessions.hub.heart.org/server/routes/dynamic-page.js
@@ -1,8 +1,14 @@
 const { withDynamicPage } = require('@parameter1/base-cms-marko-web/middleware');
 const queryFragment = require('@ascend-media/package-global/graphql/fragments/dynamic-page');
+const galleryStyleQueryFragment = require('@ascend-media/package-global/graphql/fragments/gallery-style-page');
 const page = require('@ascend-media/package-global/templates/dynamic-page');
+const galleryStyle = require('@ascend-media/package-global/templates/dynamic-page/gallery-style');
 
 module.exports = (app) => {
+  app.get('/page/:alias(award-winners-test)', withDynamicPage({
+    template: galleryStyle,
+    queryFragment: galleryStyleQueryFragment,
+  }));
   app.get('/page/:alias(*)', withDynamicPage({
     template: page,
     queryFragment,
