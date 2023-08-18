7917ed83ee2853e184afcbc3ad97d9fc102a0acb
Shinsina <shinsina@att.net>
2023-08-18 08:33:48 -0500

    Update route for applicable Global package sites

--- sites/sessions.hub.heart.org/server/routes/dynamic-page.js
+++ sites/sessions.hub.heart.org/server/routes/dynamic-page.js
@@ -5,9 +5,10 @@ const page = require('@ascend-media/package-global/templates/dynamic-page');
 const galleryStyle = require('@ascend-media/package-global/templates/dynamic-page/gallery-style');
 
 module.exports = (app) => {
-  app.get('/page/:alias(award-winners-test)', withDynamicPage({
+  app.get(/^\/page\/(awards.+)/, withDynamicPage({
     template: galleryStyle,
     queryFragment: galleryStyleQueryFragment,
+    aliasResolver: (req) => (req.params[0]),
   }));
   app.get('/page/:alias(*)', withDynamicPage({
     template: page,
