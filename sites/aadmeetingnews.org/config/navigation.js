cdde7ac82ae830207dd5e8909c338327882508d1
Jessica Wade <jessica.wade@parameter1.com>
2024-01-09 12:02:54 -0600

    Rearrange nav. for AAD

--- sites/aadmeetingnews.org/config/navigation.js
+++ sites/aadmeetingnews.org/config/navigation.js
@@ -10,14 +10,13 @@ const userTools = [];
 const topics = [
   { href: '/daily-coverage', label: 'Daily Coverage' },
   { href: '/industry-highlights', label: 'Industry Highlights' },
-  { href: '/product-samples', label: 'Product Samples' },
-  // REMOVE FOR SUMMER
-  // { href: '/small-exhibit-spotlight', label: 'Small Exhibit Spotlight' },
-  { href: 'https://ia2023.aad.org/sessions', label: 'Program', target: '_blank' },
   { href: '/photo-gallery', label: 'Photo Gallery' },
+  { href: '/product-samples', label: 'Product Samples' },
+  { href: '/small-exhibit-spotlight', label: 'Exhibit Spotlight' },
 ];
 
 const secondary = [
+  { href: 'https://ia2023.aad.org/sessions', label: 'Program', target: '_blank' },
   { href: 'https://www.aad.org/member/meetings-education/ia23/registration', label: 'Registration', target: '_blank' },
 ];
 
